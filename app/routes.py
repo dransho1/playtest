a268b03ed4603612897da050c4325b179a5dd6ce
David Ranshous <ranshous@quorum.us>
2019-08-28 00:17:27 -0400

    got auth working

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,53 @@
-from flask import render_template
+from flask import (
+    redirect,
+    render_template,
+    request,
+    session,
+)
+import re
+from spotipy import (
+    Credentials,
+    scopes,
+    Scope,
+    Spotify
+)
+import webbrowser
+
 from app import app
 
 @app.route('/')
-@app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    """Display homepage w/login link"""
+    return render_template('index.html', title='Home')
+
+
+@app.route('/login/')
+def login():
+    """Log user into Spotify"""
+    c = Credentials(
+        app.config['CLIENT_ID'],
+        app.config['CLIENT_SECRET'],
+        app.config['REDIRECT_URI']
+    )
+    scope = Scope(*scopes)
+    url = c.authorisation_url(scope)
+
+    # Code is the result of the redirect
+    return redirect(url, code=302)
+
+
+@app.route('/callback/')
+def code_generation():
+    """Generates token from code in browser"""
+    code = re.search(r'code=(.*)', request.environ['QUERY_STRING']).group(1)
+    c = Credentials(
+        app.config['CLIENT_ID'],
+        app.config['CLIENT_SECRET'],
+        app.config['REDIRECT_URI']
+    )
+    scope = Scope(*scopes)
+    token = c.request_access_token(code, scope)
+
+    s = Spotify(token.access_token)
+
+    return render_template('callback.html')
